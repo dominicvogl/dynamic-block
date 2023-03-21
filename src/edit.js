78538afeb436356b25c2eae029a28470386b4160
Dominic Vogl <dominic.vogl@cat-ia.de>
2023-03-21 13:01:26 +0100

    add swiper

--- src/edit.js
+++ src/edit.js
@@ -1,9 +1,17 @@
 import { __ } from "@wordpress/i18n";
+// import React, { useRef, useState } from "react";
 import { useBlockProps } from "@wordpress/block-editor";
 import { useSelect } from "@wordpress/data";
 import { RawHTML } from "@wordpress/element";
 import { format, dateI18n, getSettings } from "@wordpress/date";
 import "./editor.scss";
+
+// swiper JS
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
 
 export default function Edit({ attributes }) {
 	const { numberOfPosts, displayFeaturedImage } = attributes;
@@ -25,49 +33,69 @@ export default function Edit({ attributes }) {
 	console.log(posts);
 
 	return (
-		<ul {...useBlockProps()}>
-			{posts &&
-				posts.map((post) => {
-					const featuredImage =
-						post._embedded &&
-						post._embedded["wp:featuredmedia"] &&
-						post._embedded["wp:featuredmedia"].length > 0 &&
-						post._embedded["wp:featuredmedia"][0];
+		<section {...useBlockProps()}>
+			<Swiper
+				modules={[Navigation]}
+				navigation={true}
+				spaceBetween={50}
+				slidesPerView={3}
+				onSlideChange={() => console.log("slide change")}
+				onSwiper={(swiper) => console.log(swiper)}
+			>
+				{posts &&
+					posts.map((post) => {
+						const featuredImage =
+							post._embedded &&
+							post._embedded["wp:featuredmedia"] &&
+							post._embedded["wp:featuredmedia"].length > 0 &&
+							post._embedded["wp:featuredmedia"][0];
 
-					return (
-						<li key={post.id}>
-							{displayFeaturedImage && featuredImage && (
-								<img
-									src={
-										featuredImage.media_details.sizes.medium
-											.source_url
-									}
-									alt={featuredImage.alt_text}
-								/>
-							)}
-							<h3>
-								<a href={post.link}>
-									{post.title.rendered ? (
-										<RawHTML>{post.title.rendered}</RawHTML>
-									) : (
-										__("No title", "dynamic-block")
+						return (
+							<SwiperSlide key={post.id}>
+								<div>
+									{displayFeaturedImage && featuredImage && (
+										<img
+											src={
+												featuredImage.media_details
+													.sizes.medium.source_url
+											}
+											alt={featuredImage.alt_text}
+										/>
 									)}
-								</a>
-							</h3>
-							{post.date_gmt && (
-								<time dateTime={format("c", post.date_gmt)}>
-									{dateI18n(
-										getSettings().formats.date,
-										post.date_gmt
+									<h3>
+										<a href={post.link}>
+											{post.title.rendered ? (
+												<RawHTML>
+													{post.title.rendered}
+												</RawHTML>
+											) : (
+												__("No title", "dynamic-block")
+											)}
+										</a>
+									</h3>
+									{post.date_gmt && (
+										<time
+											dateTime={format(
+												"c",
+												post.date_gmt
+											)}
+										>
+											{dateI18n(
+												getSettings().formats.date,
+												post.date_gmt
+											)}
+										</time>
 									)}
-								</time>
-							)}
-							{post.excerpt.rendered && (
-								<RawHTML>{post.excerpt.rendered}</RawHTML>
-							)}
-						</li>
-					);
-				})}
-		</ul>
+									{post.excerpt.rendered && (
+										<RawHTML>
+											{post.excerpt.rendered}
+										</RawHTML>
+									)}
+								</div>
+							</SwiperSlide>
+						);
+					})}
+			</Swiper>
+		</section>
 	);
 }
